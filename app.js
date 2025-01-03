19c771e60d6e71927f27f4aa5ea9e717cd5974a6
Vinz072 <lordkevin404@gmail.com>
2025-01-03 10:19:21 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+// Fetch Requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
